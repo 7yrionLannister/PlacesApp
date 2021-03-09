511b192a912ff925fbce9299754f8d0df7efb199
Daniel Fernández <alejo.dafr@gmail.com>
2021-03-09 17:20:17 -0500

    designed navigation bar

--- app/src/main/java/co/edu/icesi/placesapp/MainActivity.java
+++ app/src/main/java/co/edu/icesi/placesapp/MainActivity.java
@@ -3,7 +3,11 @@ package co.edu.icesi.placesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/** <a target="_blank" href="https://icons8.com/icons/set/shop-department">Department Shop icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+ * <a target="_blank" href="https://icons8.com/icons/set/map-marker--v1">Map Marker icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+ * <a target="_blank" href="https://icons8.com/icons/set/marker--v1">Marker icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+ * <a target="_blank" href="https://icons8.com/icons/set/treasure-map">Treasure Map icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+ * */
 public class MainActivity extends AppCompatActivity {
 
     @Override
